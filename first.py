e71cfdd49f47a4939823e2206ee2590d87ecf38e
leejy23 <105205327+leejy23@users.noreply.github.com>
2022-10-06 23:01:53 +0900

    No.1

---
+++ first.py
@@ -0,0 +1,23 @@
+
+#print("안녕하시렵니까")
+#print("hello"*4)
+#print(12+3)
+#print(3.13+24)
+#print("안녕"+"하신가?")
+#print("더하기: ",10+23)
+
+#name = "이재영"
+#age = 23
+#print(name,age)
+
+#num = int(input("숫자: "))
+#print("결과:",num)
+
+test = False
+test2 = False
+if test:
+  print("if문 실행")
+elif test2:
+  print("elif문 실행")
+else:
+  print("else문 실행")
